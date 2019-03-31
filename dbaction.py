9d50fec63a4fcd922f98d88157fd16f3d5b40de9
Houwenda <houwd123@126.com>
2019-03-31 19:41:26 +0800

    test and fix bugs
    runable

--- dbaction.py
+++ dbaction.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 import psycopg2
 
+dbport = "5432"
+
+
 # try connecting to database
 def connectPostgresql():
-    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port="6432" )
+    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port=dbport )
     print('connect succeeded')
 
 # create 2 tables
@@ -19,7 +22,7 @@ def connectPostgresql():
                 5   delivering, cargo has been sucessfully picked up, but not delievered to Receiver
 """
 def createTable():
-    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port="6432" )
+    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port=dbport )
     cursor = conn.cursor()
     print('connect succeeded')
     cursor.execute('''create table public.users(
@@ -45,7 +48,7 @@ PS varchar(256)
 
 # insert a row into users
 def insertUser(uid, radd, pnt=100):
-    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port="6432" )
+    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port=dbport )
     cursor = conn.cursor()
     cursor.execute("insert into public.users(UserID, Raddress, Points) \
 values(%s, %s, %s)", (uid, radd, pnt))
@@ -55,22 +58,22 @@ values(%s, %s, %s)", (uid, radd, pnt))
 
 # insert a row into orders
 # -1 format not correct
-def insertOrder(sts, did, rid, tm, cg, rw, ps):
+def insertOrder(oid, sts, did, rid, tm, cg, rw, ps):
     if (sts == 2) and (rid == None):
         return -1
     if (sts == 3) and (did == None):
         return -1
-    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port="6432" )
+    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port=dbport )
     cursor = conn.cursor()
     cursor.execute("insert into public.orders(OrderID, Status, DID, RID, Time, Cargo, Reward, PS) \
-values(%s, %s, %s, %s, to_timestamp(%s, 'YYYY:MON:DD:HH24:MI:SS'), %s, %s, %s)",(sts, did, rid,tm,cg, rw, ps))
+values(%s, %s, %s, %s, to_timestamp(%s, 'YYYY:MON:DD:HH24:MI:SS'), %s, %s, %s)",(oid, sts, did, rid,tm,cg, rw, ps))
     conn.commit()
     conn.close()
     return 0
 
 # select user data from users by id
 def selectUserById(uid):
-    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port="6432" )
+    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port=dbport )
     cursor = conn.cursor()
     cursor.execute("select UserId, Raddress, Points from public.users where UserId=%s",(uid,))
     row = cursor.fetchall()
@@ -81,22 +84,22 @@ def selectUserById(uid):
     #print('UserID:', row[0][0],' Raddress:', row[0][1], ' Points:', row[0][2])
     return row[0][0], row[0][1], row[0][2]
 
-# select order data by id 
+# select order data by id
 def selectOrderById(oid):
-    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port="6432" )
+    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port=dbport )
     cursor = conn.cursor()
-    cursor.execute("select OrderID, Status, DID, RID, Time, Cargo, Reward PS from public.orders where OrderID=%s", (oid,))
+    cursor.execute("select OrderID, Status, DID, RID, Time, Cargo, Reward, PS from public.orders where OrderID=%s", (oid,))
     row = cursor.fetchall()
     conn.close()
     if row == []:
         print('no such order')
-        return None, None, None, None, None, None, None, None
-    print('OrderID:', row[0][0],' Status:', row[0][1], ' DID:', row[0][2], ' RID:', row[0][3], 'Time', row[0][4],' Cargo:', row[0][5], ' Reward:',  row[0][6], ' PS:', row[0][7])
+        return None
+#    print('OrderID:', row[0][0],' Status:', row[0][1], ' DID:', row[0][2], ' RID:', row[0][3], 'Time', row[0][4],' Cargo:', row[0][5], ' Reward:',  row[0][6], ' PS:', row[0][7])
     return row[0][0], row[0][1], row[0][2], row[0][3], row[0][4], row[0][5], row[0][6], row[0][7]
 
 # select order data by status, in order of time desc
 def selectOrderByStatus(sts):
-    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port="6432" )
+    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port=dbport )
     cursor = conn.cursor()
     cursor.execute("select OrderID, Status, DID, RID, to_char(Time, 'YYYY:MON:DD:HH24:MI:SS'), Cargo, Reward, PS from public.orders where Status=%s order by Time desc", (sts,))
     rows = cursor.fetchall()
@@ -105,7 +108,7 @@ def selectOrderByStatus(sts):
 
 # select order data where time value is ealier than the given value
 def selectOrderBeforeTime(tm):
-    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port="6432" )
+    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port=dbport )
     cursor = conn.cursor()
     cursor.execute("select OrderID, Status, DID, RID, to_char(Time, 'YYYY:MON:DD:HH24:MI:SS'), Cargo, Reward, PS from public.orders where Time<to_timestamp(%s, 'YYYY:MON:DD:HH24:MI:SS')", (tm,))
     rows = cursor.fetchall()
@@ -113,7 +116,7 @@ def selectOrderBeforeTime(tm):
     return rows # rows is possible to be empty
 
 def selectOrderByStatusBeforeTime(tm, sts):
-    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port="6432" )
+    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port=dbport )
     cursor = conn.cursor()
     cursor.execute("select OrderID, Status, DID, RID, to_char(Time, 'YYYY:MON:DD:HH24:MI:SS'), Cargo, Reward, PS from public.orders where Time<to_timestamp(%s, 'YYYY:MON:DD:HH24:MI:SS') and Status=%s", (tm, sts))
     rows = cursor.fetchall()
@@ -122,56 +125,57 @@ def selectOrderByStatusBeforeTime(tm, sts):
 
 # @Issue how setup the cargo filter?
 def selectOrderByStatusBeforeTimeByCargo(cg, tm, sts):
-    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port="6432" )
+    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port=dbport )
     cursor = conn.cursor()
     cursor.execute("select OrderID, Status, DID, RID, to_char(Time, 'YYYY:MON:DD:HH24:MI:SS'), Cargo, Reward, PS from public.orders where Time<to_timestamp(%s, 'YYYY:MON:DD:HH24:MI:SS') and Status=%s", (tm, sts))
     rows = cursor.fetchall()
     conn.close()
-    return rows # rows is possible to be empty   
+    return rows # rows is possible to be empty
 
 
 
-# update orders 
+
+# update orders
 def updateOrdersStatusById(oid, sts):
-    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port="6432" )
+    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port=dbport )
     cursor = conn.cursor()
-    cursor.execute("update public.orders set Status=%s where OrderID=%s", sts, oid)
+    cursor.execute("update public.orders set Status=%s where OrderID=%s", (sts, oid))
     conn.commit()
     conn.close()
 def updateOrdersCargoById(oid, cg):
-    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port="6432" )
+    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port=dbport )
     cursor = conn.cursor()
-    cursor.execute("update public.orders set Cargo=%s where OrderID=%s", cg, oid)
+    cursor.execute("update public.orders set Cargo=%s where OrderID=%s", (cg, oid))
     conn.commit()
     conn.close()
 def updateOrdersDIDById(oid, did):
-    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port="6432" )
+    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port=dbport )
     cursor = conn.cursor()
-    cursor.execute("update public.orders set DID=%s where OrderID=%s", did, oid)
+    cursor.execute("update public.orders set DID=%s where OrderID=%s", (did, oid))
     conn.commit()
     conn.close()
 def updateOrdersRIDById(oid, rid):
-    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port="6432" )
+    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port=dbport )
     cursor = conn.cursor()
-    cursor.execute("update public.orders set RID=%s where OrderID=%s", rid, oid)
+    cursor.execute("update public.orders set RID=%s where OrderID=%s", (rid, oid))
     conn.commit()
     conn.close()
 # update users
 def updateUsersPointsById(uid, pnt):
-    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port="6432" )
+    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port=dbport )
     cursor = conn.cursor()
-    cursor.execute("update public.orders set Points=%s where UserId=%s", pnt, uid)
+    cursor.execute("update public.users set Points=%s where UserId=%s", (pnt, uid))
     conn.commit()
     conn.close()
 
 # delete orders by id
 def deleteOrdersById(oid):
-    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port="6432" )
+    conn = psycopg2.connect(database = "postgres", user = "postgres",password = "postgres", host = "127.0.0.1", port=dbport )
     cursor = conn.cursor()
     cursor.execute("select OrderID, Status, DID, RID, to_char(Time, 'YYYY:MON:DD:HH24:MI:SS'), Cargo, Reward, PS from public.orders where OrderID=%s", (oid,))
     row = cursor.fetchall()
     if row != []:
-        cursor.execute("delete from public.orders where OrderId=%s", oid) 
+        cursor.execute("delete from public.orders where OrderId=%s", (oid,))
         conn.commit()
         conn.close()
         return row
@@ -184,15 +188,15 @@ if __name__ == "__main__":
 #    createTable()
 #    insertUser(str(123123), 'testtest', 100)
 #    insertOrder(str(456456), 0, 'delivererid', 'recieverid', '2019:MAR:27:16:10:45', '{\"size\":\"big\", \"num\":3}', '10', 'remarks')
-#    selectUserById(str(123123))
-#    selectOrderById(str(456456))
-#    print(selectOrderByStatus(0))
-#    print(selectOrderBeforeTime('2019:MAR:28:16:10:45'))
-#    print(selectOrderByStatusBeforeTime('2019:MAR:28:16:10:45',0))
+#    print(selectUserById(str(123123))) # ('123123', 'testtest', 100)
+#    print(selectOrderById(str(456456))) # ('456456', 0, 'delivererid', 'recieverid', datetime.datetime(2019, 3, 27, 16, 10, 45), {u'num': 3, u'size': u'big'}, 10, 'remarks')
+#    print(selectOrderByStatus(0)) # [('456456', 0, 'delivererid', 'recieverid', '2019:MAR:27:16:10:45', {'num': 3, 'size': 'big'}, 10, 'remarks')]
+#    print(selectOrderBeforeTime('2019:MAR:28:16:10:45')) # [('456456', 0, 'delivererid', 'recieverid', '2019:MAR:27:16:10:45', {'num': 3, 'size': 'big'}, 10, 'remarks')]
+#    print(selectOrderByStatusBeforeTime('2019:MAR:28:16:10:45',0)) # [('456456', 0, 'delivererid', 'recieverid', '2019:MAR:27:16:10:45', {'num': 3, 'size': 'big'}, 10, 'remarks')]
 #    updateOrdersStatusById(str(456456), 1)
 #    updateOrdersCargoById(str(456456), '{}')
 #    updateOrdersDIDById(str(456456), '789789')
-#    updateOrdresRIDById(str(456456), '789789')
+#    updateOrdersRIDById(str(456456), '789789')
 #    updateUsersPointsById(str(123123), 200)
 #    deleteOrdersById(str(456456))
     pass
